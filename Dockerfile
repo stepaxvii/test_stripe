d9b5e24447ecba0e559aa984fe2ab2e5aad47ce0
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-03-01 02:42:06 +0300

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
+RUN python manage.py collectstatic --no-input
+
 COPY . .
 
 EXPOSE 8000
--- test_stripe/settings.py
+++ test_stripe/settings.py
@@ -116,9 +116,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 STATIC_URL = '/django/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
