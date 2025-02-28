d3d0cb2e0cafefa20ff177bf6266f40da9946cf4
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-03-01 02:20:26 +0300

    Initial commit

--- test_stripe/settings.py
+++ test_stripe/settings.py
@@ -116,9 +116,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-STATIC_URL = '/django/static/'
-STATIC_ROOT = '/home/django/staticfiles/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
